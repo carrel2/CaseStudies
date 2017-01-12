57ddbac367cd8d40e5eae305b8687ecd309de418
carrel2 <carrel2@illinois.edu>
2017-01-12 14:40:05 -0600

    Add user accounts, login, logout, register

--- src/AppBundle/Controller/RegisterController.php
+++ src/AppBundle/Controller/RegisterController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 class RegisterController extends Controller
 {
 	/**
-	 * @Route("/register", name="registration")
+	 * @Route("/registration", name="registration")
 	 */
 	public function registerAction(Request $request)
 	{
