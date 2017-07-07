ee54d1eadf25f37969f360e0fbcd81f387b3063b
unknown <carrel2@CVM-P10F85728.ad.uillinois.edu>
2017-07-07 14:26:53 -0500

    commit

--- web/js/hotspots.js
+++ web/js/hotspots.js
@@ -52,15 +52,15 @@ $(function(){
 		},
 		onSelectChange: function(img, selection) {
 			for(s in selections) {
-				if( selection.x1 <= selections[s][1][0] ) {
+				if( selection.x1 <= selections[s][1][0] && selection.y1 < selections[s][0][1] && selection.y2 > selections[s][1][1] ) {
 					selection.x1 += 1;
-				} else if( selection.x2 >= selections[s][0][0] ) {
+				} else if( selection.x2 >= selections[s][0][0] && selection.y1 < selections[s][0][1] && selection.y2 > selections[s][1][1] ) {
 					selection.x2 -= 1;
 				}
 
-				if( selection.y1 <= selections[s][1][1] ) {
+				if( selection.y1 <= selections[s][1][1] && selection.x2 < selections[s][0][0] && selection.x1 > selections[s][1][0] ) {
 					selection.y1 += 1;
-				} else if( selection.y2 >= selections[s][0][1] ) {
+				} else if( selection.y2 >= selections[s][0][1] && selection.x2 < selections[s][0][0] && selection.x1 > selections[s][1][0] ) {
 					selection.y2 -= 1;
 				}
 			}
