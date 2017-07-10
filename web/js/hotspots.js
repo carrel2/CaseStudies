327056205de9d07fb0127fd40fff5da5a427702d
unknown <carrel2@CVM-P10F85728.ad.uillinois.edu>
2017-07-10 12:46:01 -0500

    commit

--- web/js/hotspots.js
+++ web/js/hotspots.js
@@ -58,15 +58,14 @@ $(function(){
 		onSelectChange: function(img, selection) {
 			for(s in selections) {
 				if( selection.x1 <= selections[s][1][0] && selection.x2 > selections[s][1][0] && ( selection.y1 > selections[s][0][1] && selection.y1 < selections[s][1][1] || selection.y2 > selections[s][0][1] && selection.y2 < selections[s][1][1] ) ) {
-					inst.cancelSelection();
+					inst.setSelection(selection.x1 + 1, selection.y1, selection.x2, selection.y2);
 				} else if( selection.x2 >= selections[s][0][0] && selection.x1 < selections[s][0][0] && ( selection.y1 > selections[s][0][1] && selection.y1 < selections[s][1][1] || selection.y2 > selections[s][0][1] && selection.y2 < selections[s][1][1] ) ) {
-					inst.cancelSelection();
-				}
+					inst.setSelection(selection.x1, selection.y1, selection.x2 - 1, selection.y2);				}
 
 				if( selection.y1 <= selections[s][1][1] && selection.y2 > selections[s][1][1] && ( selection.x1 > selections[s][0][0] && selection.x1 < selections[s][1][0] || selection.x2 > selections[s][0][0] && selection.x2 < selections[s][1][0] ) ) {
-					inst.cancelSelection();
+					inst.setSelection(selection.x1, selection.y1 + 1, selection.x2, selection.y2);
 				} else if( selection.y2 >= selections[s][0][1] && selection.y1 < selections[s][0][1] && ( selection.x1 > selections[s][0][0] && selection.x1 < selections[s][1][0] || selection.x2 > selections[s][0][0] && selection.x2 < selections[s][1][0] ) ) {
-					inst.cancelSelection();
+					inst.setSelection(selection.x1 + 1, selection.y1, selection.x2, selection.y2 - 1);
 				}
 			}
 			$('#debug').children(':last-child').text( selection.x1 + ', ' + selection.y1);
