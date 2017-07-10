5e318d176217abed54a6eeacea09f03135055ccf
unknown <carrel2@CVM-P10F85728.ad.uillinois.edu>
2017-07-10 13:28:54 -0500

    commit

--- web/js/hotspots.js
+++ web/js/hotspots.js
@@ -60,9 +60,8 @@ $(function(){
 				if( selection.x1 <= selections[s][1][0] && selection.x2 > selections[s][1][0] && ( selection.y1 > selections[s][0][1] && selection.y1 < selections[s][1][1] || selection.y2 > selections[s][0][1] && selection.y2 < selections[s][1][1] ) ) {
 					inst.setSelection(selections[s][1][0] + 1, selection.y1, selections[s][1][0] + (selection.x2 - selection.x1) + 1, selection.y2);
 				} else if( selection.x2 >= selections[s][0][0] && selection.x1 < selections[s][0][0] && ( selection.y1 > selections[s][0][1] && selection.y1 < selections[s][1][1] || selection.y2 > selections[s][0][1] && selection.y2 < selections[s][1][1] ) ) {
-					inst.setSelection(selections[s][0][0] - (selection.x2 - selection.x1) - 1, selection.y1, selections[s][0][0] - 1, selection.y2);				}
-
-				if( selection.y1 <= selections[s][1][1] && selection.y2 > selections[s][1][1] && ( selection.x1 > selections[s][0][0] && selection.x1 < selections[s][1][0] || selection.x2 > selections[s][0][0] && selection.x2 < selections[s][1][0] ) ) {
+					inst.setSelection(selections[s][0][0] - (selection.x2 - selection.x1) - 1, selection.y1, selections[s][0][0] - 1, selection.y2);
+				} else if( selection.y1 <= selections[s][1][1] && selection.y2 > selections[s][1][1] && ( selection.x1 > selections[s][0][0] && selection.x1 < selections[s][1][0] || selection.x2 > selections[s][0][0] && selection.x2 < selections[s][1][0] ) ) {
 					inst.setSelection(selection.x1, selections[s][1][1] + 1, selection.x2, selections[s][1][1] + (selection.y2 - selection.y1) + 1);
 				} else if( selection.y2 >= selections[s][0][1] && selection.y1 < selections[s][0][1] && ( selection.x1 > selections[s][0][0] && selection.x1 < selections[s][1][0] || selection.x2 > selections[s][0][0] && selection.x2 < selections[s][1][0] ) ) {
 					inst.setSelection(selection.x1, selections[s][0][1] - (selection.y2 - selection.y1) - 1, selection.x2, selections[s][0][1] - 1);
