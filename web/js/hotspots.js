2bc493a29991225c178b093d1dd5a9fd21e48bd1
unknown <carrel2@CVM-P10F85728.ad.uillinois.edu>
2017-07-10 14:33:52 -0500

    quick fix

--- web/js/hotspots.js
+++ web/js/hotspots.js
@@ -56,7 +56,7 @@ $(function(){
 			}
 		},
 		onSelectChange: function(img, selection) {
-			var newX1 selection.x1, newX2 = selection.x2, newY1 = selection.y1, newY2 = selection.y2;
+			var newX1 = selection.x1, newX2 = selection.x2, newY1 = selection.y1, newY2 = selection.y2;
 
 			for(s in selections) {
 				if( selection.x1 <= selections[s][1][0] && selection.x2 > selections[s][1][0] && ( selection.y1 > selections[s][0][1] && selection.y1 < selections[s][1][1] || selection.y2 > selections[s][0][1] && selection.y2 < selections[s][1][1] ) ) {
