1056dabc902486ffc396a905c67b71888f58f94f
unknown <carrel2@CVM-P10F85728.ad.uillinois.edu>
2017-07-25 13:28:38 -0500

    allow super user to change user roles, otherwise the user's password is needed

--- src/AppBundle/Form/UserType.php
+++ src/AppBundle/Form/UserType.php
@@ -63,6 +63,7 @@ class UserType extends AbstractType
 
 				$form->add('plainPassword', PasswordType::class, array(
 					'label' => 'Old Password',
+					'required' => $currentUser->getRole() != 'ROLE_SUPER_USER',
 				));
 
 				$form->add('newPassword', RepeatedType::class, array(
