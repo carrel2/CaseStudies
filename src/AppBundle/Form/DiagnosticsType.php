cad452800fa8e71527a3e103d1c5c735f8322337
Brandon Carrel <carrel2@www.vetmed.illinois.edu>
2018-07-23 13:38:53 -0500

    pull DiagnosticsType

--- src/AppBundle/Form/DiagnosticsType.php
+++ src/AppBundle/Form/DiagnosticsType.php
@@ -21,10 +21,10 @@ class DiagnosticsType extends AbstractType
 				'multiple' => true,
 				'label' => false,
 				'choice_attr' => function(DiagnosticProcedure $d, $key, $index) {
-					return ['class' => 'test', 'data-cost' => $d->getPerDayCost(), 'data-dosage' => $d->getDosage(), 'data-interval' => $d->getDosageInterval()];
+					return ['class' => 'test', 'data-cost' => $d->getCost()];
 				},
 				'group_by' => function($val, $key, $index) {
-					return $val->getGroup();
+					return $val->getGroupName();
 				},
 			))
 			->add('submit', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', array(
