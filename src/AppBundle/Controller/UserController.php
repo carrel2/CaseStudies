0b2e6b9ac8ccab824cc22edc90ac01643d870742
carrel2 <carrel2@illinois.edu>
2018-08-31 13:45:29 -0500

    add ability to view and download results as pdf

--- src/AppBundle/Controller/UserController.php
+++ src/AppBundle/Controller/UserController.php
@@ -6,6 +6,9 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use TFox\MpdfPortBundle\Response\PDFResponse;
+use Mpdf\Mpdf;
+use AppBundle\Entity\Results;
 use AppBundle\Entity\User;
 
 class UserController extends Controller
@@ -21,5 +24,20 @@ class UserController extends Controller
 		return $this->render('Default/results.html.twig', array(
 			'results' => $results,
 		));
+	}
+
+	/**
+	 * @Route("/user/pdf/{results}", name="resultsToPdf")
+	 */
+	public function pdfAction(Request $r, Results $results)
+	{
+		$pdf = new Mpdf();
+
+		$pdf->WriteHTML(file_get_contents('css/bulma.css'), 1);
+		$pdf->WriteHTML($this->renderView('PDF/result.html.twig', array(
+			'result' => $results,
+		)), 2);
+
+		return new PDFResponse($pdf->Output('tmp.pdf', \Mpdf\Output\Destination::STRING_RETURN));
 	}
 }
